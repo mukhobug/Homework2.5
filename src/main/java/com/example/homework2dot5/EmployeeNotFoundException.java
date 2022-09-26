b5f2dc6355bc6e59339a07d9169f567e348ee5a8
Alejandro <mukhobug@gmail.com>
2022-09-26 17:48:30 +0300

    a

--- src/main/java/com/example/homework2dot5/EmployeeNotFoundException.java
+++ src/main/java/com/example/homework2dot5/EmployeeNotFoundException.java
@@ -1,5 +1,9 @@
 package com.example.homework2dot5;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class EmployeeNotFoundException extends RuntimeException {
     public EmployeeNotFoundException(String message) {
         super(message);
